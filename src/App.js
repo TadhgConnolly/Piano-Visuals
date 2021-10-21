296905474be693978fc20d6ff0f0cecb1f370138
Tadhg <tadhg@pop-os.localdomain>
2021-10-22 10:56:05 +1300

    buttton plays note audio setup

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { useEffect } from "react";
+import "./App.css";
+import AudioPlayer from "./Keyboard/AudioPlayer";
+function App() {
+  const audioPlayer = AudioPlayer();
+
+  useEffect(() => {
+    audioPlayer.setInstrument("acoustic_grand_piano");
+  }, []);
+
+  const handleClick = () => {
+    audioPlayer.playNote("C4");
+  };
+
+  return (
+    <div className="app-container">
+      <button onClick={handleClick}>Play</button>
+    </div>
+  );
+}
+
+export default App;
