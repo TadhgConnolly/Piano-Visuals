d2fe1c4b96a17c5317ba6066c8a14c0ee0253774
Tadhg <tadhg@pop-os.localdomain>
2021-10-28 09:58:03 +1300

    centered piano add josh effects

--- src/Effects/Effect.js
+++ src/Effects/Effect.js
@@ -1,18 +1,91 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Sketch from 'react-p5'
 
-function Effect () {
+function Effect() {
+
+    const [p5, setP5] = useState(null);
+    const [canvas, setCanvas] = useState(null);
+    
+    var width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+    var height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+    var fillColor;
+    var fade;
+
+    var bubbles = []
+
     const setup = (p5, canvasParentRef) => {
-        p5.createCanvas(500, 400).parent(canvasParentRef)
+        p5.noStroke();
+        fade = 255
+
+        setP5(p5);
+        setCanvas(p5.createCanvas(width, height).parent(canvasParentRef));
+        //rectWidth = 100 / 4;
+        p5.background(0, 0, 0)
+        fillColor = p5.color(200, 10, 25, fade)
     }
 
     const draw = p5 => {
-        p5.background(255, 130, 20)
-        p5.ellipse(100, 100, 100)
-        p5.ellipse(300, 100, 100)
-      }
+        for (const bubble of bubbles) {            
+            if (bubble.startFade === true) {
+                p5.background(0, 0, 0)
+                if (bubble.fade > 0) {
+                    bubble.fade = bubble.fade - 20;
+                } else if (bubble.fade <= 0) {
+                    bubble.fade = 0;
+                } 
+            }
+            p5.fill(p5.color(bubble.red, bubble.green, bubble.blue, bubble.fade))
+            p5.ellipse(bubble.x, bubble.y, bubble.radius)  
+        }
 
-      return <Sketch setup={setup} draw={draw} />
+    }
+
+    const onKeyPressed = (e) => {
+        let red = Math.round(Math.random() * 255);
+        let green = Math.round(Math.random() * 255);
+        let blue = Math.round(Math.random() * 255);
+        let bubble = new Bubble(Math.round(Math.random() * (width - 100)), Math.round(Math.random() * (height - 100)), 100, red, green, blue, e.keyCode, false)
+        bubbles.push(bubble)
+        //console.log(e.keyCode)
+        //p5.ellipse(bubble.x, bubble.y, bubble.radius)
+        //p5.fill(bubble.fill)
+    };
+
+    const onKeyUp = (e) => { // p5.background(100,100,0);
+        console.log(e.keyCode)
+        for (const bubble of bubbles) {
+            if (bubble.keyCode === e.keyCode) {
+                bubble.startFade = true;
+            }
+        }
+        //bubbles.shift.startFade = true;
+        //p5.background(Math.round(Math.random() * 100), Math.round(Math.random() * 100), Math.round(Math.random() * 100))
+        //p5.ellipse(Math.round(Math.random() * 100), Math.round(Math.random() * 100), Math.round(Math.random() * 100))
+        //fillColor = p5.color(Math.round(Math.random() * 255), Math.round(Math.random() * 255), Math.round(Math.random() * 255), fade)
+        //p5.fill(fillColor)
+        //p5.ellipse(Math.round(Math.random() * (width - 100)), Math.round(Math.random() * (height - 100)), 100)
+        //console.log(e)
+    };
+
+    return <Sketch setup={setup} draw={draw} keyPressed={onKeyPressed} keyReleased={onKeyUp} />
 }
+
+class Bubble {
+
+    constructor(x, y, radius, red, green, blue, keyCode, startFade) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        
+        this.fade = 255;
+        this.red = red;
+        this.green = green;
+        this.blue = blue;
+
+        this.startFade = startFade;
+        this.keyCode = keyCode;
+    }
+}
+
 
 export default Effect
