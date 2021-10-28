f325ae5a3e910363f34cb5afbcbaa1cf6b51511f
Tadhg <tadhg@pop-os.localdomain>
2021-10-29 12:22:28 +1300

    working on effects

--- src/Effects/Effect.js
+++ src/Effects/Effect.js
@@ -10,7 +10,6 @@ function Effect() {
     var height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
     var fillColor;
     var fade;
-
     var bubbles = []
 
     const setup = (p5, canvasParentRef) => {
@@ -25,24 +24,29 @@ function Effect() {
     }
 
     const draw = p5 => {
-        for (const bubble of bubbles) {            
+        for (const bubble of bubbles) {                 
             if (bubble.startFade === true) {
                 p5.background(0, 0, 0)
                 if (bubble.fade > 0) {
                     bubble.fade = bubble.fade - 20;
                 } else if (bubble.fade <= 0) {
                     bubble.fade = 0;
-                } 
+                }
             }
+            if (bubble.radius < 100) {
+                bubble.radius += 10
+            } else bubble.radius += 3
+            
+            
             p5.fill(p5.color(bubble.red, bubble.green, bubble.blue, bubble.fade))
-            //p5.ellipse(bubble.x, bubble.y, bubble.radius)
-            p5.translate(500, 300)
+            
+            p5.ellipse(bubble.x, bubble.y, bubble.radius)
             p5.noStroke()
             for (let i = 0; i < 10; i ++) {
-                p5.ellipse(0, 30, 20, 80);
+                // p5.translate(500, 200)
+                p5.ellipse(bubble.x, bubble.y, (bubble.radius / 4), bubble.radius);
                 p5.rotate(Math.PI/5);
               }
-
         }
         if (bubbles.length > 4) {
             bubbles.shift()
@@ -51,12 +55,16 @@ function Effect() {
     }
 
     const onKeyPressed = (e) => {
-        let red = Math.round(Math.random() * 255);
-        let green = Math.round(Math.random() * 255);
-        let blue = Math.round(Math.random() * 255);
-        let bubble = new Bubble(Math.round(Math.random() * (width - 100)), Math.round(Math.random() * (height - 100)), (Math.random() * 80 + 20), red, green, blue, e.keyCode, false)
-        bubbles.push(bubble)
-        console.log(bubbles)
+        if (e.keyCode === 49 || e.keyCode === 52 || e.keyCode === 56 || e.keyCode === 71) {
+            return
+        } else if ((e.keyCode >= 48 && e.keyCode <= 90) || e.keyCode === 222 || (e.keyCode >= 188 && e.keyCode <= 191)) {
+            let red = Math.round(Math.random() * 255);
+            let green = Math.round(Math.random() * 255);
+            let blue = Math.round(Math.random() * 255);
+            let bubble = new Bubble(Math.round(Math.random() * (width - 100) + 100), Math.round(Math.random() * (height - 100) + 100), 2, red, green, blue, e.keyCode, false)
+            bubbles.push(bubble)
+            }
+        //console.log(bubbles)
         //console.log(e.keyCode)
         //p5.ellipse(bubble.x, bubble.y, bubble.radius)
         //p5.fill(bubble.fill)
@@ -89,7 +97,7 @@ class Bubble {
         this.y = y;
         this.radius = radius;
         
-        this.fade = 255;
+        this.fade = 127;
         this.red = red;
         this.green = green;
         this.blue = blue;
